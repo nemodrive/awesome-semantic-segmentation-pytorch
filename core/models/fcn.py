c7b1a5e6f598fb30ae8c753126a5414e6edacc82
AndreiMihalea <mihalea.andrei@gmail.com>
2021-03-18 22:18:30 +0200

    Tried eval with tanh

--- core/models/fcn.py
+++ core/models/fcn.py
@@ -156,13 +156,14 @@ def get_fcn32s(dataset='pascal_voc', backbone='vgg16', pretrained=False, root='~
         'ade20k': 'ade',
         'coco': 'coco',
         'citys': 'citys',
+        'kitti': 'kitti',
     }
     from ..data.dataloader import datasets
     model = FCN32s(datasets[dataset].NUM_CLASS, backbone=backbone, pretrained_base=pretrained_base, **kwargs)
     if pretrained:
         from .model_store import get_model_file
         device = torch.device(kwargs['local_rank'])
-        model.load_state_dict(torch.load(get_model_file('fcn32s_%s_%s' % (backbone, acronyms[dataset]), root=root),
+        model.load_state_dict(torch.load(get_model_file('fcn32s_%s_%s_soft_correct' % (backbone, acronyms[dataset]), root=root),
                               map_location=device))
     return model
 
--- scripts/eval.py
+++ scripts/eval.py
@@ -63,7 +63,7 @@ class Evaluator(object):
         self.metric = SegmentationMetric(val_dataset.num_class)
 
     def eval(self):
-        for threshold in np.arange(0.425, 0.425, 0.025):          
+        for threshold in np.arange(-0.25, 0.25, 0.025):          
             self.metric.reset()
             self.model.eval()
             if self.args.distributed:
@@ -82,8 +82,9 @@ class Evaluator(object):
                 with torch.no_grad():
                     outputs = model(image)
                 
-                logits = nn.Sigmoid()(outputs[0][0][0])
+                logits = nn.Tanh()(outputs[0][0][0])
                 logits = logits.cpu().data.numpy()
+                softmax = logits.copy()
 
                 # print(target.shape, logits.shape, image.shape, filename)
             
@@ -99,31 +100,38 @@ class Evaluator(object):
                 sum_acc += pixAcc
                 sum_miou += mIoU
 
-                logger.info("Sample: {:d}, validation pixAcc: {:.3f}, mIoU: {:.3f}".format(
-                        i + 1, pixAcc * 100, mIoU * 100))
+                #logger.info("Sample: {:d}, validation pixAcc: {:.3f}, mIoU: {:.3f}".format(
+                #        i + 1, pixAcc * 100, mIoU * 100))
 
 
-                plt.figure(figsize=(6.4, 2.88), dpi=100)
-                plt.gca().set_axis_off()
-                plt.subplots_adjust(top=1, bottom=0, right=1, left=0,
-                                hspace=0, wspace=0)
-                plt.margins(0, 0)
-                plt.gca().xaxis.set_major_locator(plt.NullLocator())
-                plt.gca().yaxis.set_major_locator(plt.NullLocator())
-                sns.heatmap(logits, cbar=True, xticklabels=True, yticklabels=True)
+                #plt.figure(figsize=(6.4, 2.88), dpi=100)
+                #plt.gca().set_axis_off()
+                #plt.subplots_adjust(top=1, bottom=0, right=1, left=0,
+                #                hspace=0, wspace=0)
+                #plt.margins(0, 0)
+                #plt.gca().xaxis.set_major_locator(plt.NullLocator())
+                #plt.gca().yaxis.set_major_locator(plt.NullLocator())
+                #sns.heatmap(softmax, cbar=True, xticklabels=True, yticklabels=True)
                 #plt.show()
                 #plt.waitforbuttonpress()
                 #plt.close()
 
-                save_path = os.path.join(outdir, '\\'.join(filename[0].split('/')[-3:]))
-                plt.savefig(save_path)
+                #save_path = os.path.join(outdir, '\\'.join(filename[0].split('/')[-3:]))
+                #plt.savefig(save_path.replace('pred_pic', 'pred_sigmoid'))
+                #plt.close()
 
                 if self.args.save_pred:
-                    pred = torch.argmax(outputs[0], 1)
-                    pred = pred.cpu().data.numpy()
+                    mask_overlay = np.array(logits)
+                    mask_overlay = np.array([mask_overlay] * 3)
+                    mask_overlay[mask_overlay != 0] = 255
+                    mask_overlay[mask_overlay != 255] = 0
+                    mask_overlay[0, :, :] = 0
+                    mask_overlay[2, :, :] = 0
 
-                    predict = pred.squeeze(0)
-                    mask = get_color_pallete(predict, self.args.dataset)
+                    #img = cv2.imread(filename[0])
+                    #img = cv2.resize(img, (832, 256))[:, 96:-96, :]
+                    #res_img = cv2.addWeighted(mask_overlay.transpose((1, 2, 0)).astype(np.float32), 1., img.astype(np.float32), 1, 0)
+                    #cv2.imwrite(os.path.join(outdir, '\\'.join(filename[0].split('/')[-3:])), res_img)
                     # print(outdir, os.path.splitext(filename[0])[0] + '.png')
                     # np.save(os.path.join(outdir, os.path.splitext(filename[0])[0] + '.npy'), outputs[0][0][0].cpu().data.numpy())
                     #mask.save(os.path.join(outdir, os.path.splitext(filename[0])[0] + '.png'))
@@ -149,7 +157,7 @@ if __name__ == '__main__':
     # TODO: optim code
     args.save_pred = True
     if args.save_pred:
-        outdir = '../runs/pred_sigmoid/{}_{}_{}'.format(args.model, args.backbone, args.dataset)
+        outdir = '../runs/pred_pic/{}_{}_{}'.format(args.model, args.backbone, args.dataset)
         if not os.path.exists(outdir):
             os.makedirs(outdir)
 
